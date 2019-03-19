d1e8fc79c4cefeb865cce8b7c200ac709fd7bd21
Jordan Higgins <jordandhiggins@gmail.com>
2019-03-18 18:10:11 -0600

    refactored a bit

--- scraping/scrape.js
+++ scraping/scrape.js
@@ -1,77 +1,82 @@
-// https://www.google.com/search?q=
-// block youtube, allrecipes, foodnetwork
-// make custom for https://tastesbetterfromscratch.com/
-const request = require('request'), 
-cheerio = require('cheerio');
-
+const request = require('request'), cheerio = require('cheerio');
 function searchHeadersForSelectors($) {
     let instructions = null;
     let ingredients = null;
+    // 7 because there are header tags h1 through h6, search all htags
     for (let i = 1; i < 7; i++) {
-        // console.log(`h${i}`);
         $(`h${i}`).each(function(i, el) {
             if ($(this).text().toLowerCase().includes("instructions") || $(this).text().toLowerCase().includes("directions") 
             || $(this).text().toLowerCase().includes("steps") || $(this).text().toLowerCase().includes("make")) {
                 let newInstructions = $(this).parent();
-                if (newInstructions.children().length < 2) {
+                if (newInstructions.children().length < 2) { // if the parent only has one child, we need to grab its parent to get the content we want
                     newInstructions = newInstructions.parent();
                 }
                 instructions = chooseBestInstructions(instructions, newInstructions);
             }
             if ($(this).text().toLowerCase().includes("ingredients")) {
                 let newIngredients = $(this).parent();
-                if (newIngredients.children().length < 2) {
+                if (newIngredients.children().length < 2) { // same as above
                     newIngredients = newIngredients.parent();
                 }
-                ingredients = chooseBestIngredients(instructions, newIngredients);
+                ingredients = chooseBestIngredients(ingredients, newIngredients);
             }
         });
     }
     return { instructions: instructions, ingredients: ingredients };
 }
-function searchDivsForSelectors($, instructions, ingredients) {
-    console.log("finding ingredient and direction selectors in divs...");
-    $('div').each(function(i, el) {
+function searchTagForSelectors($, tagType, instructions, ingredients) {
+    console.log("finding ingredient and direction selectors in different tag types...");
+    $(`${tagType}`).each(function(i, el) {
         if (($(this).text().toLowerCase().includes("instructions") || $(this).text().toLowerCase().includes("directions") 
         || $(this).text().toLowerCase().includes("steps") || $(this).text().toLowerCase().includes("make")) && $(this).text().length < 30) {
             let newInstructions = $(this).parent();
-            if (newInstructions.children().length < 2) { // try a while loop to break nested (one element crap)
+            if (newInstructions.children().length < 2) { // if the parent only has one child, we need to grab its parent to get the content we want
                 newInstructions = newInstructions.parent();
             }
             instructions = chooseBestInstructions(instructions, newInstructions); 
         }
         if ($(this).text().toLowerCase().includes("ingredients") && $(this).text().length < 15) {
             let newIngredients = $(this).parent();
-            if (newIngredients.children().length < 2) {
+            if (newIngredients.children().length < 2) { // same as above
                 newIngredients = newIngredients.parent();
             }
-            ingredients = chooseBestIngredients(instructions, newIngredients); 
+            ingredients = chooseBestIngredients(ingredients, newIngredients); 
         }
     });
 }
 function chooseBestInstructions(current, noob) {
-    if (!current) {
+    if (!current) { // if there isn't an established instruction selector, start with this one
         return noob;
     }
+    // if the previous tag was selected because of the keywords "make" or "step", which are less acurate than "directions" and "instructions", switch selector to the new one
     if ((!current.text().toLowerCase().includes("instructions") || !current.text().toLowerCase().includes("directions"))
     && (noob.text().toLowerCase().includes("instructions") || noob.text().toLowerCase().includes("directions"))) {
         return noob;
     }
+    // This is to help eliminate grabbing the erroneous (and common) "Did you make this recipe? Tell us about it @instagramHandle"
     if (current.text().includes("@") && !noob.text().includes("@")) {
         return noob;
     }
+    // This assumption is based purely on what I've seen while creating this, but the shorter .text() is usually the more accurate one. 
     if (current.text().length > noob.text().length && !noob.text().includes("@")) {
         return noob;
     }
     return current;
 }
 function chooseBestIngredients(current, noob) {
-    if (!current) {
+    if (!current) { // start with first selector identified. 
         return noob;
     }
+    // the selector with content including common ingredient measurements is better
+    if (noob.text().includes("tsp") || noob.text().includes("oz") || noob.text().includes("cup") ||
+    noob.text().includes("teaspoon") || noob.text().includes("ounce")) {
+        return noob;
+    }
+    // "<" indicate the scraper has picked up a html tag, which happens while looking for ingredients for some reasons. This = no bueno.
     if (current.text().includes("<") && !noob.text().includes("<")) {
         return noob;
     }
+    // shorter content tends to be better, based solely on my observations
     if (noob.text().length < current.text().length) {
         return noob;
     }
@@ -88,39 +93,37 @@ async function searchForRecipe(url) {
                 reject(error);
             }
             const $ = cheerio.load(body);
-            // console.log(body);
             let recipeObj = {
                 url: url,
             }
             let selectors = searchHeadersForSelectors($);
+            // if recipe isn't found by searching through the htags, seach divs.
             if (!selectors.instructions || !selectors.ingredients) {
-                searchDivsForSelectors($, selectors.instructions, selectors.ingredients);
+                searchTagForSelectors($, "div", selectors.instructions, selectors.ingredients);
             }
-            // recipeObj.ingredientSelector = chooseBestSelector(selectors.ingredients);
-            // recipeObj.instructSelector = chooseBestSelector(selectors.instructions);
+            // if recipe isn't found by searching through the htags, seach ptags.
+            if (!selectors.instructions || !selectors.ingredients) {
+                searchTagForSelectors($, "p", selectors.instructions, selectors.ingredients);
+            }
             recipeObj.instructions = selectors.instructions;
             recipeObj.ingredients = selectors.ingredients;
             if (!recipeObj.ingredients || !recipeObj.instructions) {
                 resolve(null);
             } else {
-                // console.log(recipeObj.ingredientSelector.text());
                 resolve(recipeObj);
             }
         });
     });
 }
-async function visitURLs(urls) {
+async function scanSitesForRecipe(urls) {
     let recipeObjArr = [];
-    // for (let i = 0; i < urls.length; i++) {
-    for (let i = 0; i < 1; i++) {
-        console.log("searching... " + urls[i]);
+    for (let i = 0; i < urls.length; i++) {
+        console.log(`Searching ${urls[i]}...`);
         try {
             await searchForRecipe(urls[i]).then((result) => {
                 if (result) {
                     recipeObjArr.push(result);
-                } else {
-                    console.log("Recipe NOT found...");
-                }
+                } 
             });
         } catch(error) {
             console.log(`ERROR: ${error}`);
@@ -134,44 +137,68 @@ async function visitURLs(urls) {
         }
     });
 }
-module.exports = function searchGoogleForURLs(query) {
-    request(`http://www.google.com/search?q=${query} blog recipe`, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        // console.log('body:', body); // Print the HTML for the Google homepage.
-        const $ = cheerio.load(body),
-        // initialize variables
-        links = $(".r a"), urls = [], sitesToAvoid = ["foodnetwork", "allrecipes"];
-        links.each(function (i, link) {
-            let url = $(link).attr("href"); // get the href attribute of each link
-            url = url.replace("/url?q=", "").split("&")[0]; // strip out unnecessary junk
-            if (url.charAt(0) === "/") {
-                return;
+module.exports = async function searchGoogleForURLs(query) {
+    return new Promise((resolve, reject) => {
+        request(`http://www.google.com/search?q=${query} blog recipe`, function (error, response, body) {
+            if (error) {
+                console.log('error:', error); // Print the error if one occurred
             }
-            const siteName = url.split(".")[1];
-            if (!sitesToAvoid.includes(siteName)) { 
-                urls.push(url);
-            } else {
-                console.log("Avoiding site");
-            }
-        });
-        console.log("urls found: " + urls.length);
-        let promiseOfRecipes = visitURLs(urls);
-        promiseOfRecipes.then((result) => {
-            console.log(`recipes found: ${result.length}.`);
-            for (let i = 0; i < result.length; i++) {
-                if (result[i].instructions.text() == result[i].ingredients.text()) {
-                    console.log(`${i}. SITE ${result[i].url}`);
-                    console.log(`${i}. BOTH BOTH ${result[i].instructions.text()}`);
-                } else {
-                    console.log(`${i}. SITE ${result[i].url}`);
-                    console.log(`${i}. INSTRUCTIONS ${result[i].instructions.text()}`);
-                    console.log(`${i}. INGREDIENTS ${result[i].ingredients.text()}`);
+            const $ = cheerio.load(body),
+            links = $(".r a"), sitesToAvoid = ["foodnetwork", "allrecipes", "youtube", "badURL"], urls = [], recipes = [];
+            links.each(function (i, link) {
+                let url = $(link).attr("href"); // get the href attribute of each link
+                url = url.replace("/url?q=", "").split("&")[0]; // strip out unnecessary junk
+                // if (url.charAt(0) === "/") {
+                //     return;
+                // }
+                let siteName = "badURL";
+                if (url.includes(".")) {
+                    siteName = url.split(".")[1];
+                } 
+                if (!sitesToAvoid.includes(siteName)) { // to avoid sites I don't want to visit
+                    urls.push(url);
+                } 
+            });
+            // console.log("urls found: " + urls.length);
+            let promiseOfRecipes = scanSitesForRecipe(urls);
+            promiseOfRecipes.then((result) => {
+                console.log(`recipes found: ${result.length}.`);
+                for (let i = 0; i < result.length; i++) {
+                    if (result[i].instructions.text() == result[i].ingredients.text()) {
+                        let instructionStr = "", children = result[i].instructions.children();
+                        for (let j = 0; j < children.length; j++) {
+                            instructionStr += children.eq(j).text().trim() + "\n";
+                        }
+                        recipes.push({
+                            url: result[i].url,
+                            instructions: instructionStr,
+                            ingredients: null
+                        });
+                    } else {
+                        let instructionStr = "", ingredientStr = "", 
+                        childInstructions = result[i].instructions.children(), 
+                        childIngredients = result[i].ingredients.children();
+                        for (let j = 0; j < childInstructions.length; j++) {
+                            instructionStr += childInstructions.eq(j).text().trim() + "\n";
+                        }
+                        for (let j = 0; j < childIngredients.length; j++) {
+                            ingredientStr += childIngredients.eq(j).text().trim() + "\n";
+                        }
+                        recipes.push({
+                            url: result[i].url,
+                            instructions: instructionStr,
+                            ingredients: ingredientStr
+                        });
+                    }
                 }
-            }
-        }).catch((error) => {
-            console.log(error);
+                if (!recipes) {
+                    resolve(null);
+                } else {
+                    resolve(recipes);
+                }
+            }).catch((error) => {
+                console.log(error);
+            }); 
         });
-        
     });
 }
